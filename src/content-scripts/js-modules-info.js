fef5fc275589677acefcbf6a6ccc15f5fee0e215
tfennelly <tom.fennelly@gmail.com>
2017-03-26 08:27:28 +0100

    add a comment to the content script for loading the js script urls

--- src/content-scripts/js-modules-info.js
+++ src/content-scripts/js-modules-info.js
@@ -2,6 +2,7 @@ const message = require('./../messaging');
 
 const scripts = [];
 
+// Gather all the script URLs from the page.
 const scriptElements = document.getElementsByTagName("script");
 for (let i = 0; i < scriptElements.length; i++) {
     const scriptElement = scriptElements[i];
@@ -11,6 +12,7 @@ for (let i = 0; i < scriptElements.length; i++) {
     }
 }
 
+// Send them to the panel.
 message.sendMessageToPanel('js.modules.info', {
     scripts: scripts
 });
