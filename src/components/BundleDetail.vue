eacb36b16747b39ecce6a8f4dc4330680d7fdac5
tfennelly <tom.fennelly@gmail.com>
2017-03-31 12:08:26 +0100

    bundle details overview

--- src/components/BundleDetail.vue
+++ src/components/BundleDetail.vue
@@ -1,6 +1,32 @@
 <template>
     <div class="bundleDetail">
-        {{bundle.loadEvent.bundleId}} - {{bundle.bundleDetails.script}}
+        <h4>Overview</h4>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td><code>{{bundle.loadEvent.bundleId}}</code></td>
+            </tr>
+            <tr>
+                <td>Script</td>
+                <td><code>{{bundle.bundleDetails.script}}</code></td>
+            </tr>
+            <tr>
+                <td>Created</td>
+                <td>{{new Date(decoded.created).toString()}}</td>
+            </tr>
+            <tr>
+                <td>js-builder</td>
+                <td><code>{{decoded.jsBuilderVer}}</code></td>
+            </tr>
+            <tr v-if="decoded.hpiPluginId">
+                <td>Loaded from</td>
+                <td>{{decoded.hpiPluginId}}</td>
+            </tr>
+            <tr>
+                <td># modules</td>
+                <td>{{numModules(decoded.moduleDefs)}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -10,6 +36,23 @@
             bundle: Object
         },
         methods: {
+            numModules: function(moduleDefs) {
+                let count = 0;
+                for (const moduleName in moduleDefs) {
+                    if (moduleDefs.hasOwnProperty(moduleName)) {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        },
+        computed: {
+            decoded: function() {
+                if (!this.bundle.bundleDetails.decoded) {
+                    this.bundle.bundleDetails.decoded = JSON.parse(this.bundle.bundleDetails.data);
+                }
+                return this.bundle.bundleDetails.decoded;
+            }
         }
     }
 </script>
@@ -17,5 +60,9 @@
 <style>
     .bundleDetail {
         padding: 10px;
+    }
+    .bundleDetail td:first-child {
+        padding-right: 10px;
+        font-weight: bold;
     }
 </style>
