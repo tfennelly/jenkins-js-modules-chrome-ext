233d70627c24812251b4812ae94a0223346ea2a5
tfennelly <tom.fennelly@gmail.com>
2017-04-01 13:34:53 +0100

    own impl of a bootstrap alert
    the normal one uses a &times; entity that displayes weird in the dev tools

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,3 +1,7 @@
+<!--
+Our own impl of the bootstrap alert. The one provided uses a &times; entity in the
+dismiss button, which displays screwed up in the dev tools as something like "A~".
+-->
 <template>
     <div :class="alertClasses" role="alert" v-if="!dismiss">
         <button v-if="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close" @click="dismiss=true">
