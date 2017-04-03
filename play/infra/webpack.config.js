0a5a344a31f6abb0b92bb5444dd13bb91691247d
tfennelly <tom.fennelly@gmail.com>
2017-04-03 10:54:40 +0100

    add font-awesome

--- play/infra/webpack.config.js
+++ play/infra/webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     plugins: [
         new CopyWebpackPlugin([
             { from: 'dist/bootstrap/css/bootstrap.css', to: 'css' },
+            { from: 'dist/font-awesome', to: 'font-awesome' },
             { from: 'play/sample-data', to: 'sample-data' }
         ])
     ]
