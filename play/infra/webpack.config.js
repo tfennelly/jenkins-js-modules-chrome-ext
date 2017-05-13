cdc27ac7e629914a532abfc277fa1993b9548812
tfennelly <tom.fennelly@gmail.com>
2017-05-13 10:28:10 +0100

    failed attempt to configure sourcemaps

--- play/infra/webpack.config.js
+++ play/infra/webpack.config.js
@@ -1,6 +1,10 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
+//
+// TODO: get sourcemaps working ... webpack bundles are unreadable without it.
+//
 module.exports = {
+    devtool: 'source-map',
     plugins: [
         new CopyWebpackPlugin([
             { from: 'dist/panel.css' },
